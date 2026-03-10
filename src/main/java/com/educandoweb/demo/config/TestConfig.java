62ff580180d67223587df83b23ac9aa5e831a734
Elizandra Fernandes <luis.f@academico.ifs.edu.br>
2026-03-10 15:25:22 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/demo/config/TestConfig.java
+++ src/main/java/com/educandoweb/demo/config/TestConfig.java
@@ -23,7 +23,8 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
     @Autowired
-     private OrderItemRepository orderItemRepository;
+    private OrderItemRepository orderItemRepository;
+
     @Override
     public void run(String... args) throws Exception {
         Category cat1 = new Category(null, "Electronics");
@@ -60,6 +61,10 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
         OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
-       orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1= new Payment(null,Instant.parse("2019-06-20T19:53:07Z"),o1);
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
     }
 }
