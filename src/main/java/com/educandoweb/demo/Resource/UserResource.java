f7ece5c3e1014737e37b2f65eb9be294a032123b
Elizandra Fernandes <luis.f@academico.ifs.edu.br>
2026-03-10 23:36:26 -0300

    User delete

--- src/main/java/com/educandoweb/demo/Resource/UserResource.java
+++ src/main/java/com/educandoweb/demo/Resource/UserResource.java
@@ -33,5 +33,10 @@ public class UserResource {
                 .path("/{id}").buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).body(user);
     }
+ @DeleteMapping (value = "/{id}")
+ public ResponseEntity<Void> delete(@PathVariable Long id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+ }
 
 }
--- src/main/java/com/educandoweb/demo/service/UserService.java
+++ src/main/java/com/educandoweb/demo/service/UserService.java
@@ -3,8 +3,10 @@ package com.educandoweb.demo.service;
 import com.educandoweb.demo.entities.User;
 import com.educandoweb.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,6 +26,15 @@ public class UserService {
 
     public User insert (User user){
         return userRepository.save(user);
+    }
+    public void delete(long id){
+        try {
+            userRepository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new RuntimeException("User not found");
+        }
+
     }
 
 }
